f502472710db3d2b080c7a30e256c86dff46d27c
Michael Fawzy <ahetofawzy@gmail.com>
2026-06-24 13:19:18 +0300

    final

--- eks.tf
+++ eks.tf
@@ -1,19 +1,5 @@
-module "ebs_csi_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-v2"
-  version = "~> 5.0"
-
-  role_name             = "ebs-csi-driver-role"
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-}
-
 module "eks" {
+
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
@@ -23,21 +9,19 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
 
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
-    }
-  }
+  enable_cluster_creator_admin_permissions = true
 
   access_entries = {
     terraform_user = {
+
       principal_arn = "arn:aws:iam::933428634359:user/Terraform-user"
+
       policy_associations = {
         admin = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
           access_scope = {
             type = "cluster"
           }
@@ -47,11 +31,15 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
+
     main = {
-      desired_size   = 3
-      min_size       = 2
-      max_size       = 4
+
+      desired_size = 3
+      min_size     = 2
+      max_size     = 4
+
       instance_types = ["t3.small"]
+
     }
   }
 }
